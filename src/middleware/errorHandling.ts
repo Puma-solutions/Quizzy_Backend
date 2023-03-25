1c508aeacfce6c6be72d89cca5b5594c2a05a6b8
CharlyMoreno <carlitosmoreno12@hotmail.com>
2023-03-25 12:36:25 -0300

    Fix function for error handling

--- src/middleware/errorHandling.ts
+++ src/middleware/errorHandling.ts
@@ -1,9 +1,14 @@
 import { Request,Response,NextFunction } from "express"
+import { AppError } from "../utils/AppError"
 
 import { logger } from "../utils/logger"
 
 export const errorHandling = (error:Error,req:Request,res:Response,next:NextFunction) => {
-    logger.error(`${req.method} url:: ${req.url} --- ${error.message}`)
     res.header("Content-Type", 'application/json')
-    res.status(500).send({error:error.message})
+    if(error instanceof AppError){
+        res.status(error.status).json({error:error.message})    
+    }else{
+        logger.error(`${req.method} url:: ${req.url} --- [ERROR]: ${error.message}`)
+        res.status(500).json({error:error.message})
+    }
 }
