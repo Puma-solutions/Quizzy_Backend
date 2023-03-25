9a4ed3c9f798fa799bf5d82c05e2df5d0fa5e591
CharlyMoreno <carlitosmoreno12@hotmail.com>
2023-03-25 12:35:06 -0300

    Add User Dao

--- src/daos/user.dao.ts
+++ src/daos/user.dao.ts
@@ -1,8 +1,12 @@
-import { modelUser,User } from "../models/user.model";
+import { userModel,User } from "../models/user.model";
 import { BaseDao } from "./base.dao";
 
 export class UserDao extends BaseDao<User>{
     constructor(){
-        super(modelUser)
+        super(userModel)
+    }
+    async getUserByEmail(email:string){
+        const user = await this.collection.findOne({email:email})
+        return user
     }
 }
