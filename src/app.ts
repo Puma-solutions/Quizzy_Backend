c74b3e29abd8302e4b8faf7f6fcbcf562987a817
CharlyMoreno <carlitosmoreno12@hotmail.com>
2023-03-25 12:37:13 -0300

    Fix app.ts

--- src/app.ts
+++ src/app.ts
@@ -1,15 +1,19 @@
 import express from "express"
+import { authMiddleware } from "./middleware/auth.middleware"
 import {errorHandling} from "./middleware/errorHandling"
 
 import { authRouter } from "./routes/auth.routes"
 
 const app = express()
 
-app.use(errorHandling)
+app.use(express.json())
+app.use(express.urlencoded({ extended: true }))
 app.use('/auth',authRouter)
 
-app.get('/',(req,res)=>{
+app.get('/',authMiddleware,(req,res)=>{
     res.send('Hola Mundo')
 })
+
+app.use(errorHandling)
 
 export default app;
