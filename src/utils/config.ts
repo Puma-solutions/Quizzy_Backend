5c9288cb43e3b11eae2a56281c1c11c80a279f8a
CharlyMoreno <carlitosmoreno12@hotmail.com>
2023-03-25 12:36:38 -0300

    Add secret word config

--- src/utils/config.ts
+++ src/utils/config.ts
@@ -3,4 +3,5 @@ dotenv.config();
 
 export const PORT = process.env.PORT || 8080;
 export const MONGOURL = process.env.MONGOURL || ''
+export const SECRET = process.env.SECRET || 'palabraMuySecreta'
 
