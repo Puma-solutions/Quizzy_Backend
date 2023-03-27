5b66faf22321c9588fb511f78835a17249764d75
CharlyMoreno <carlitosmoreno12@hotmail.com>
2023-03-27 13:09:15 -0300

    Add subject and exam routes in app.ts

--- src/app.ts
+++ src/app.ts
@@ -3,12 +3,16 @@ import { authMiddleware } from "./middleware/auth.middleware"
 import {errorHandling} from "./middleware/errorHandling"
 
 import { authRouter } from "./routes/auth.routes"
+import {subjectsRoutes} from "./routes/subjects.routes"
+import { examRoutes } from "./routes/exam.routes"
 
 const app = express()
 
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
 app.use('/auth',authRouter)
+app.use('/subjects',subjectsRoutes)
+app.use('/exam',examRoutes)
 
 app.get('/',authMiddleware,(req,res)=>{
     res.send('Hola Mundo')
